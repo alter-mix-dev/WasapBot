26f23ec6a2df358128618e25021702e543ff331f
alter-mix-dev <alter.mix.dev@gmail.com>
2026-09-11 10:07:28 -0600

    Change access token in server.js
    Updated access token for the application.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // Claves de configuración de tu aplicación de Meta
-const TOKEN_DE_ACCESO = "EAAdzFrUZCZAVQBSdJoAcZARZADXPL93JX8O0J8kbsSAhEeCof5Ls80C8CvjVVsL9TN1YoubRWScOEPh4FA81ZAdQyyden8StGRR7jpzWdo1AI8bzmVMuxJudwr7SZBlUhhJ8JCqdVFVtqI0ZCXFQRytRALdzrRSpinxNBxh3AjinlHLkPMsdfGbDgNKwlCUaar8JA9Kh6mjEIoUL5MNbjiApKHEgZARSDjPTsf1s4lboLNiisB4ZA9T4hdEATJzKudjts0iEH3hTBfwyiFlOZBUJoc";
+const TOKEN_DE_ACCESO = "EAAdzFrUZCZAVQBSeffbZC7ckZCmvMKb7BXvNfkQ3zvfh5W5RlsSpm5ZBfxbT2EoiCtO9ZAxCqt5ZAgqhwbw29uXqKzGi4eZCFUb8dQTQCoehwzoa6jRRIITqidfpTQ4GcHwz37ZBFyHd7lMn6i0EFTrYAjq6jt20WMSQCDpEyi9i0xU2fr8icKZBMc2DseDKZApuBAa4jjiR4zTcZC0SIlSl3qXySeMuQdWvjZCPLiZCf3czk4iTELlTC6blCZBQY80ntJ0uxwYIaNbIqRATWHjPtDZARza7";
 const ID_TELEFONO_BUSINESS = "1378623791993907";
 const TOKEN_VERIFICACION_WEBHOOK = "WasapBot"; // Tú inventas esta palabra
 
